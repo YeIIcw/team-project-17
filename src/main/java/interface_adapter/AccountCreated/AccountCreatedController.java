e62b470ca3bd4b30b55d5f7cc2e07a8fe848b0b2
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:16:47 -0500

    AccountCreatedController implemented

--- src/main/java/interface_adapter/AccountCreated/AccountCreatedController.java
+++ src/main/java/interface_adapter/AccountCreated/AccountCreatedController.java
@@ -1,4 +1,15 @@
 package interface_adapter.AccountCreated;
 
+import interface_adapter.HomeScreen.HomeScreenViewModel;
+import view.AccountCreatedView;
+import view.HomeScreenView;
+
 public class AccountCreatedController {
+
+    public void execute() {
+        AccountCreatedView accountCreatedView = new AccountCreatedView(new AccountCreatedViewModel());
+
+        HomeScreenView homeScreenView = new HomeScreenView(new HomeScreenViewModel());
+        homeScreenView.display();
+    }
 }
