21005529f4d368a698ca71c99b2f1d1490bd82c8
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:16:31 -0500

    HomeScreenController implemented

--- src/main/java/interface_adapter/HomeScreen/HomeScreenController.java
+++ src/main/java/interface_adapter/HomeScreen/HomeScreenController.java
@@ -1,4 +1,24 @@
 package interface_adapter.HomeScreen;
 
+import interface_adapter.Login.LoginViewModel;
+import interface_adapter.Signup.SignUpViewModel;
+import view.HomeScreenView;
+import view.LoginView;
+import view.SignupView;
+
 public class HomeScreenController {
+
+    public void goToLogin() {
+        HomeScreenView homeScreenView = new HomeScreenView(new HomeScreenViewModel());
+
+        LoginView loginView = new LoginView(new LoginViewModel());
+        loginView.display();
+    }
+
+    public void goToCreateAccount() {
+        HomeScreenView homeScreenView = new HomeScreenView(new HomeScreenViewModel());
+
+        SignupView signupView = new SignupView(new SignUpViewModel());
+        signupView.display();
+    }
 }
