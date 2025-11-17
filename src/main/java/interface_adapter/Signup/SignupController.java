84a04ca447116da1cd7928bb0e4d67d063d687ac
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:13:30 -0500

    SignupController

--- src/main/java/interface_adapter/Signup/SignupController.java
+++ src/main/java/interface_adapter/Signup/SignupController.java
@@ -1,4 +1,27 @@
-package interface_adapter.signup;
+package interface_adapter.Signup;
+
+import interface_adapter.AccountCreated.AccountCreatedViewModel;
+import use_case.signup.SignupInteractor;
+import view.AccountCreatedView;
+import view.SignupView;
+
+import java.io.IOException;
 
 public class SignupController {
+
+    private String username;
+    private String password;
+
+    public void execute(String username, String password) throws IOException {
+        this.username = username;
+        this.password = password;
+
+        SignupInteractor signupInteractor = new SignupInteractor();
+        signupInteractor.execute(username, password);
+
+        SignupView signupView = new SignupView(new SignUpViewModel());
+
+        AccountCreatedView accountCreatedView = new AccountCreatedView(new AccountCreatedViewModel());
+        accountCreatedView.display();
+    }
 }
