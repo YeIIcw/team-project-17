52613899842eb81b01c7af677b840c5e92ed1d43
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:08:29 -0500

    LoginView implemented

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,4 +1,102 @@
 package view;
 
+import interface_adapter.Login.LoginController;
+import interface_adapter.Login.LoginViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LoginView {
+
+    private LoginViewModel loginViewModel;
+
+    private final JFrame frame = new JFrame();
+    private final JLabel usernameLabel = new JLabel("Username:");
+    private final JLabel passwordLabel = new JLabel("Password:");
+    private final JTextField usernameText = new JTextField(10);
+    private final JPasswordField passwordText = new JPasswordField(10);
+    private final JButton loginButton = new JButton("Login");
+
+    public LoginView(LoginViewModel loginViewModel) {
+        this.loginViewModel = loginViewModel;
+
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.add(usernameLabel);
+        usernamePanel.add(usernameText);
+
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.add(passwordLabel);
+        passwordPanel.add(passwordText);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(usernamePanel);
+        mainPanel.add(passwordPanel);
+        mainPanel.add(loginButton);
+
+        frame.add(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource().equals(loginButton)) {
+                    LoginController loginController = new LoginController();
+
+                    try {
+                        JFrame newFrame = new JFrame("Incorrect username or password!");
+                        if (!checkLogin(usernameText.getText(), passwordText.getText())) {
+                            JPanel incorrect = new JPanel();
+                            incorrect.setLayout(new BoxLayout(incorrect, BoxLayout.Y_AXIS));
+                            JLabel incorrectLabel = new JLabel("Incorrect username or password");
+                            incorrectLabel.setForeground(Color.RED);
+                            incorrect.add(incorrectLabel);
+                            mainPanel.add(incorrect);
+
+                            newFrame.add(mainPanel);
+
+                            newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            newFrame.setLocationRelativeTo(null);
+                            newFrame.pack();
+                            newFrame.setVisible(true);
+                        }
+                        else {
+                            loginController.execute(usernameText.getText(), passwordText.getText());
+                            frame.dispose();
+                            newFrame.dispose();
+                        }
+
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    public boolean checkLogin(String username, String password) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("src/main/java/scratch.txt")));
+        String line = br.readLine();
+
+        while (line != null) {
+            String[] fields = line.split(", ");
+            if (fields[0].equals(username) && fields[1].equals(password)) {
+                return true;
+            }
+            line = br.readLine();
+        }
+
+        return false;
+    }
 }
