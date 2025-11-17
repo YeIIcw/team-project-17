610e6b7a91570a423234585f790a841c149829ae
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:12:29 -0500

    LogoutController implemented

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,4 +1,16 @@
-package interface_adapter.logout;
+package interface_adapter.Logout;
+
+import interface_adapter.HomeScreen.HomeScreenViewModel;
+import interface_adapter.Loggedin.LoggedInViewModel;
+import view.HomeScreenView;
+import view.LoggedInView;
 
 public class LogoutController {
+
+    public void execute() {
+        LoggedInView loggedInView = new LoggedInView(new LoggedInViewModel());
+
+        HomeScreenView homeScreenView = new HomeScreenView(new HomeScreenViewModel());
+        homeScreenView.display();
+    }
 }
