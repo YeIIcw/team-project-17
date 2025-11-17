0403094925dacb25534703933e658892ed3b3f61
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:17:34 -0500

    Main implemented

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,24 @@
 package app;
 
+import javax.swing.*;
+import view.HomeScreenView;
+
 public class Main {
+    public static void main(String[] args) {
+        AppBuilder appBuilder = new AppBuilder();
+        appBuilder.addHomeScreenView();
+        appBuilder.addHomeScreenUseCase();
+        appBuilder.addSignupView();
+        appBuilder.addSignupUseCase();
+        appBuilder.addLoginView();
+        appBuilder.addLoginUseCase();
+        appBuilder.addAccountCreatedView();
+        appBuilder.addAccountCreatedUseCase();
+        appBuilder.addLoggedInView();
+        appBuilder.addLoggedInUseCase();
+        appBuilder.addPreferencesView();
+        appBuilder.addPreferencesUseCase();
+
+        appBuilder.build();
+    }
 }
