c09f8f3d8998119c6596862b7b7e6178f1efc7da
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:11:43 -0500

    LoginInteractor implemented

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -1,4 +1,27 @@
 package use_case.login;
 
-public class LoginInteractor {
+import data_access.FileAccessDataObject;
+import entity.Player;
+
+import java.io.IOException;
+
+public class LoginInteractor implements LoginInputBoundary {
+
+    private Player player;
+
+    public void execute(String username, String password) throws IOException {
+        player = new Player(username, password);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayerPreferences(String category, String difficulty, String type, int numQuestions) {
+        player.setCategory(category);
+        player.setDifficulty(difficulty);
+        player.setType(type);
+        player.setNumQuestions(numQuestions);
+        System.out.println(player.getCategory() + player.getDifficulty() + player.getType() + player.getNumQuestions());
+    }
 }
