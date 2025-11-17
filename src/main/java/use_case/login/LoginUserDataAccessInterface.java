0773dacf85c2c1849007ec573c4facd1f4009128
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:11:26 -0500

    LoginUserDataAccessInterface implemented

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -1,4 +1,8 @@
 package use_case.login;
 
+import java.io.IOException;
+
 public interface LoginUserDataAccessInterface {
+
+    public void login(String username, String password) throws IOException;
 }
