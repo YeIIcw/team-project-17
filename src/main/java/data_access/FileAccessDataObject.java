2ace77da85c55dab7a27e3c1898615c179078b09
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:17:16 -0500

    FileAccessDataObject implemented

--- src/main/java/data_access/FileAccessDataObject.java
+++ src/main/java/data_access/FileAccessDataObject.java
@@ -1,4 +1,29 @@
 package data_access;
 
-public class FileAccessDataObject {
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+import java.io.*;
+
+public class FileAccessDataObject implements LoginUserDataAccessInterface,
+        SignupUserDataAccessInterface {
+
+    public void login(String username, String password) throws IOException {
+        FileWriter fw = new FileWriter("src/main/java/scratch.txt", true);
+
+        fw.write(username + ", " + password + ", 0\n");
+        fw.close();
+    }
+
+    public void signup(String username, String password) throws IOException {
+        FileWriter fw = new FileWriter("src/main/java/scratch.txt", true);
+
+        fw.write(username + ", " + password + ", 0\n");
+        fw.close();
+    }
+
+//    public void signup2(String username, String password) {
+//
+//    }
+
 }
