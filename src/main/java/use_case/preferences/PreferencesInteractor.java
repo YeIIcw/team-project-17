21eb3f3a1e5ed97eaca20d75120e442168b56c65
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:10:53 -0500

    PreferencesInteractor implemented

--- src/main/java/use_case/preferences/PreferencesInteractor.java
+++ src/main/java/use_case/preferences/PreferencesInteractor.java
@@ -1,4 +1,23 @@
 package use_case.preferences;
 
+import use_case.login.LoginInteractor;
+
 public class PreferencesInteractor {
+
+    private final String category;
+    private final String difficulty;
+    private final String types;
+    private final int numQuestions;
+
+    public PreferencesInteractor(String category, String difficulty, String types, int numQuestions) {
+        this.category = category;
+        this.difficulty = difficulty;
+        this.types = types;
+        this.numQuestions = numQuestions;
+    }
+
+    public void execute() {
+        LoginInteractor loginInteractor = new LoginInteractor();
+        loginInteractor.setPlayerPreferences(category, difficulty, types, numQuestions);
+    }
 }
