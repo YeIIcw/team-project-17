e6b48032fbc36ea63ce4efd6f62ffff79e3191be
Eliza Azaro <elizaazaro@Elizas-Laptop-4.local>
2025-11-17 10:14:07 -0500

    PreferencesController implemented

--- src/main/java/interface_adapter/Preferences/PreferencesController.java
+++ src/main/java/interface_adapter/Preferences/PreferencesController.java
@@ -1,4 +1,25 @@
 package interface_adapter.Preferences;
 
+import interface_adapter.Login.LoginController;
+import use_case.preferences.PreferencesInteractor;
+
 public class PreferencesController {
+    private final String category;
+    private final String difficulty;
+    private final String type;
+    private final int numQuestions;
+
+    public PreferencesController(String category, String difficulty, String type, int numQuestions) {
+        this.category = category;
+        this.difficulty = difficulty;
+        this.type = type;
+        this.numQuestions = numQuestions;
+    }
+
+    public void execute() {
+        LoginController loginController = new LoginController();
+//        loginController.execute();
+        loginController.setPreferences(category, difficulty, type, numQuestions);
+        //do something to move to the first question view
+    }
 }
