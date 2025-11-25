8b506773b45f113ba4ed3417cf1c48ec9d0b1094
Annie Wang <anniewang@Annies-MacBook-Air-4.local>
2025-11-25 01:00:36 -0500

    created LeaderboardInteractor

--- src/main/java/use_case/leaderboard/LeaderboardInteractor.java
+++ src/main/java/use_case/leaderboard/LeaderboardInteractor.java
@@ -1,4 +1,30 @@
 package use_case.leaderboard;
 
-public class LeaderboardInteractor {
+import entity.ScoreEntry;
+
+public class LeaderboardInteractor implements LeaderboardInputBoundary{
+    private final LeaderboardDataAccessInterface dataAccess;
+    private final LeaderboardOutputBoundary outputBoundary;
+
+    public LeaderboardInteractor(LeaderboardDataAccessInterface dataAccess,
+                                LeaderboardOutputBoundary outputBoundary) {
+        this.dataAccess = dataAccess;
+        this.outputBoundary = outputBoundary;
+    }
+
+    @Override
+    public void saveScore(LeaderboardInputData inputData) {
+        try {
+            ScoreEntry newScore = new ScoreEntry(inputData.getUsername(), inputData.getScore());
+            dataAccess.saveScore(newScore);
+
+            LeaderboardOutputData outputData =
+                    new LeaderboardOutputData("Score saved", true);
+            outputBoundary.prepareSuccessView(outputData);
+        } catch (Exception e) {
+            LeaderboardOutputData outputData =
+                    new LeaderboardOutputData("Failed to save score: " + e.getMessage(), false);
+            outputBoundary.prepareFailView(outputData);
+        }
+    }
 }
