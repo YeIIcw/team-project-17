02e5fcbbb7a0a6ca9345fa52407f3e851b8299d7
Annie Wang <anniewang@Annies-MacBook-Air-4.local>
2025-11-25 00:33:20 -0500

    created LeaderboardDataAccessInterface

---
+++ src/main/java/use_case/leaderboard/LeaderboardDataAccessInterface.java
@@ -0,0 +1,11 @@
+package use_case.leaderboard;
+
+import entity.ScoreEntry;
+
+import java.util.List;
+
+public interface LeaderboardDataAccessInterface {
+    void saveScore(ScoreEntry score);
+    List<ScoreEntry> getAllScores();
+
+}
